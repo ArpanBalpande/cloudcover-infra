c5bb92ff14429f12c7227de71dc0cd0420ac0a02
ArpanBalpande <arpan.balpande22@gmail.com>
2021-11-03 22:21:59 +0530

    reduced nodes

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -34,90 +34,35 @@ module "eks" {
       additional_userdata           = "echo foo bar"
       asg_desired_capacity          = 2
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
-    },
+    }
+  ]
+
+  # AWS Auth (kubernetes_config_map)
+  map_roles = [
     {
-      name                          = "worker-group-2"
-      instance_type                 = "t2.micro"
-      additional_userdata           = "echo foo bar"
-      asg_desired_capacity          = 1
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
+      rolearn  = "arn:aws:iam::66666666666:role/role1"
+      username = "role1"
+      groups   = ["system:masters"]
     },
   ]
 
-  # # Worker groups (using Launch Templates)
-  # worker_groups_launch_template = [
-  #   {
-  #     name                    = "spot-1"
-  #     override_instance_types = ["t2.micro"]
-  #     spot_instance_pools     = 1
-  #     asg_max_size            = 5
-  #     asg_desired_capacity    = 5
-  #     kubelet_extra_args      = "--node-labels=node.kubernetes.io/lifecycle=spot"
-  #     public_ip               = true
-  #   },
-  # ]
+  map_users = [
+    {
+      userarn  = "arn:aws:iam::66666666666:user/user1"
+      username = "user1"
+      groups   = ["system:masters"]
+    },
+    {
+      userarn  = "arn:aws:iam::66666666666:user/user2"
+      username = "user2"
+      groups   = ["system:masters"]
+    },
+  ]
 
-  # # Managed Node Groups
-  # node_groups_defaults = {
-  #   ami_type  = "AL2_x86_64"
-  #   disk_size = 50
-  # }
-
-  # node_groups = {
-  #   example = {
-  #     desired_capacity = 1
-  #     max_capacity     = 3
-  #     min_capacity     = 1
-
-  #     instance_types = ["t2.micro"]
-  #     capacity_type  = "SPOT"
-  #     k8s_labels = {
-  #       Environment = "test"
-  #       GithubRepo  = "terraform-aws-eks"
-  #       GithubOrg   = "terraform-aws-modules"
-  #     }
-  #     additional_tags = {
-  #       ExtraTag = "example"
-  #     }
-  #     taints = [
-  #       {
-  #         key    = "dedicated"
-  #         value  = "gpuGroup"
-  #         effect = "NO_SCHEDULE"
-  #       }
-  #     ]
-  #     update_config = {
-  #       max_unavailable_percentage = 50 # or set `max_unavailable`
-  #     }
-  #   }
-  # }
-
-  # # AWS Auth (kubernetes_config_map)
-  # map_roles = [
-  #   {
-  #     rolearn  = "arn:aws:iam::66666666666:role/role1"
-  #     username = "role1"
-  #     groups   = ["system:masters"]
-  #   },
-  # ]
-
-  # map_users = [
-  #   {
-  #     userarn  = "arn:aws:iam::66666666666:user/user1"
-  #     username = "user1"
-  #     groups   = ["system:masters"]
-  #   },
-  #   {
-  #     userarn  = "arn:aws:iam::66666666666:user/user2"
-  #     username = "user2"
-  #     groups   = ["system:masters"]
-  #   },
-  # ]
-
-  # map_accounts = [
-  #   "777777777777",
-  #   "888888888888",
-  # ]
+  map_accounts = [
+    "777777777777",
+    "888888888888",
+  ]
 
   tags = {
     Example    = local.name
@@ -129,9 +74,6 @@ module "eks" {
 ################################################################################
 # Supporting resources
 ################################################################################
-
-data "aws_availability_zones" "available" {
-}
 
 resource "random_string" "suffix" {
   length  = 8
